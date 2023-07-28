e753cae8c32df863ecd85dd3d1bff522171a3936
Michał Mikołajek <mmikoajek0@gmail.com>
2023-07-28 15:41:50 +0200

    Added the uncommitted changes from the past commit

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
+import Shop from './routes/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ANTAX</h1>
+        <Routes>
+          <Route path='/' element={<Navigation/>} />
+          <Route path="shop" element={<Shop />} />
+        </Routes>
       </header>
     </div>
   );
