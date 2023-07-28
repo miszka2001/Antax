cf1d928d6ac8e3383296e3d6a63adadbaaa42c67
Michał Mikołajek <mmikoajek0@gmail.com>
2023-07-28 19:55:53 +0200

    Added styled-components lib and first styling for navigation component

--- client/my-app/src/routes/navigation/navigation.component.tsx
+++ client/my-app/src/routes/navigation/navigation.component.tsx
@@ -1,11 +1,17 @@
 import { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { NavLink, NavLinks, NavigationContainer } from "./navigation.styles";
 
 const Navigation = () => {
     return (
         <Fragment>
-            <Link to="/shop">SHOP</Link>
-            <Link to="/auth">SINGIN</Link>
+            <NavigationContainer>
+                <NavLinks>
+            <NavLink to="/auth">SINGIN</NavLink>
+            <NavLink to="/shop">SHOP</NavLink>
+            </NavLinks>
+            <Outlet />
+            </NavigationContainer>
         </Fragment>
     )
 }
