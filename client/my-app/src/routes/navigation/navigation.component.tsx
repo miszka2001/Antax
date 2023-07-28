ad14dac649d9ca82628b0566d189e823aca6b226
Michał Mikołajek <mmikoajek0@gmail.com>
2023-07-28 15:37:58 +0200

    The react-dom-router is added, along with little testing to see if it works

---
+++ client/my-app/src/routes/navigation/navigation.component.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <Link to="/shop">SHOP</Link>
+        </Fragment>
+    )
+}
+
+export default Navigation;
