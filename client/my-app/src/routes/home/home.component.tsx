8a2a36926c92395b209869da745234ad5c7033bc
Michał Mikołajek <mmikoajek0@gmail.com>
2023-08-08 20:21:28 +0200

    Basic home page blueprint

--- client/my-app/src/routes/home/home.component.tsx
+++ client/my-app/src/routes/home/home.component.tsx
@@ -1,10 +1,49 @@
 import { Outlet } from "react-router-dom";
 
 const Home = () => {
-    return (<div>
-        <h1>I am home</h1>
+    return (
+    <div>
+        <div className="categories-container">
+            <div className="category-container">
+                {/* <img /> */}
+                <div className="category-body-container">
+                    <h2>T-shirt</h2>
+                    <p>Shop now</p>
+                </div>
+            </div>
+            <div className="category-container">
+                {/* <img /> */}
+                <div className="category-body-container">
+                    <h2>Trousers</h2>
+                    <p>Shop now</p>
+                </div>
+            </div>
+            <div className="category-container">
+                {/* <img /> */}
+                <div className="category-body-container">
+                    <h2>Glasses</h2>
+                    <p>Shop now</p>
+                </div>
+            </div>
+            <div className="category-container">
+                {/* <img /> */}
+                <div className="category-body-container">
+                    <h2>Jackets</h2>
+                    <p>Shop now</p>
+                </div>
+            </div>
+            <div className="category-container">
+                {/* <img /> */}
+                <div className="category-body-container">
+                    <h2>Hats</h2>
+                    <p>Shop now</p>
+                </div>
+            </div>
+
+        </div>
         <Outlet />
-    </div>)
+    </div>
+    )
 }
 
 export default Home;
