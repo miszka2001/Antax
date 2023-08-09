f0605a76df5ec479b5cfb7508fc86b72e92678f3
Michał Mikołajek <mmikoajek0@gmail.com>
2023-08-09 16:05:44 +0200

    Added a DRF and code refactored by black

--- server/antax_server/antax_server/wsgi.py
+++ server/antax_server/antax_server/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'antax_server.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "antax_server.settings")
 
 application = get_wsgi_application()
