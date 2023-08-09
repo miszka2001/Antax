2778fda4ff76c82a41b5d9a9a624defbd45d983c
Michał Mikołajek <mmikoajek0@gmail.com>
2023-08-09 18:18:10 +0200

    Urls copied from docs

--- server/antax_server/antax_server/urls.py
+++ server/antax_server/antax_server/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.jwt")),
+    path(r"^auth/", include("djoser.urls")),
+    path(r"^auth/", include("djoser.urls.jwt")),
 ]
