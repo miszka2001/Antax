b4e3c92d972773b8c9abb6f5c3d3e9396056ecb0
Michał Mikołajek <mmikoajek0@gmail.com>
2023-08-10 12:14:34 +0200

    Modification of the serializer

--- server/antax_server/authentication/serializer.py
+++ server/antax_server/authentication/serializer.py
@@ -1,9 +1,20 @@
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from authentication.models import UserAccount
 
 User = get_user_model()
 
 
-class UserCreateSerialiazer:
-    class Meta(UserCreateSerializer.Meta):
+class UserCreateSerialiazer(serializers.ModelSerializer):
+    class Meta:
         model = User
         fields = ("id", "email", "password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
