6b99fc0d8493762805fd32e1483f7354ded94288
Michał Mikołajek <mmikoajek0@gmail.com>
2023-08-10 14:24:14 +0200

    View for creating a new user

--- server/antax_server/authentication/views.py
+++ server/antax_server/authentication/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import UserCreateSerialiazer
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class NewUserCreate(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        create_serialiazer = UserCreateSerialiazer(data=request.data)
+        print(create_serialiazer)
+        if create_serialiazer.is_valid():
+            new_user = create_serialiazer.save()
+            if new_user:
+                return Response(status=status.HTTP_201_CREATED)
+        return Response(create_serialiazer.errors, status=status.HTTP_400_BAD_REQUEST)
